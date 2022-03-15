f8736756130c9cca5e784c16ce2ce888b70ef60c
Moji <m0ji19881122@gmail.com>
2022-03-15 20:19:29 +0900

    Initial Commit

---
+++ HowToUseGitHub/HowToUseGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseGitHubApp.swift
+//  HowToUseGitHub
+//
+//  Created by MacBook Air M1 on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
