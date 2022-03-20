45b5d265ad40099737c7f0870a1137893fc252d9
Moji-Amijo <101574333+Moji-Amijo@users.noreply.github.com>
2022-03-20 15:39:44 +0900

    挨拶を変更
    morningをeveningに変更

--- HowToUseGitHub/ContentView.swift
+++ HowToUseGitHub/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Good morning, JINLOG!!!")
+        Text("Good evening, JINLOG!!!")
             .padding()
     }
 }
